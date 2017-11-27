302dad27a4886d6ed08676d2ad2f6156680002ed
moriartyisntdead <artemivasyuk@mail.ru>
2017-11-27 16:24:41 +0200

    upd

--- js/script.js
+++ js/script.js
@@ -1,4 +1,31 @@
 $(function () {
+    new WOW().init();
+
+    $('.section4').parallax({imageSrc: '../img/bg.png'});
+    $('.section8').parallax({imageSrc: '../img/bg2.png'});
+
+    $(".owl-carousel").owlCarousel({
+        nav: true,
+        navText: ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
+        items: 2,
+        responsive: {
+            0: {
+                items: 1
+            },
+            426: {
+                items: 2
+            }
+        }
+    });
+
+    //[$('.am-next'),$('.am-prev')]
+
+    $('.toggle-cities, .question-title').click(function () {
+        setTimeout(function () {
+            $(window).trigger('resize');
+        }, 500);
+    });
+
     $('#sliderSum').slider({
         orientation: "horizontal",
         range: "min",
